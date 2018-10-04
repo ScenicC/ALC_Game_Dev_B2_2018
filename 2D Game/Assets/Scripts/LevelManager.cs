91bf082f3f04445fe4edcb6771ee330233a7bd19
Cort <kencan76live@gmail.com>
2018-10-04 10:28:45 -0600

    Added KillPlayer Script

--- 2D Game/Assets/Scripts/LevelManager.cs
+++ 2D Game/Assets/Scripts/LevelManager.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+    public GameObject CurrentCheckPoint;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        PC = FindObjectOfType<Rigidbody2D>();
+		}
 	
 	// Update is called once per frame
 	void Update () {
