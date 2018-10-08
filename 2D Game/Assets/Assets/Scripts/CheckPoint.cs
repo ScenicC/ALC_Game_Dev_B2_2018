171c739b00f6fec9318035f09f6801bd13c34713
Cort <kencan76live@gmail.com>
2018-10-08 10:09:56 -0600

    Added Scripts
    Added Checkpoint, LevelManager, and EnemyPatrol Scripts

---
+++ 2D Game/Assets/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+
+    public LevelManager LevelManager;
+
+	// Use this for initialization
+	void Start () {
+        LevelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.name == "PC"){
+            LevelManager.CurrentCheckPoint = gameObject;
+            Debug.Log("Activated Checkpoint" + transform.position);
+        }
+
+    }
+}
