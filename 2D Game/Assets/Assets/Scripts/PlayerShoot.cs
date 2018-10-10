38e7a33e919affb14053db3331ad4d4dcfd5fa2a
Cort <kencan76live@gmail.com>
2018-10-10 10:30:45 -0600

    Added Scripts
    Added Projectile and PlayerShoot Script and some shooting mechanics

---
+++ 2D Game/Assets/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+    
+    public Transform FirePoint;
+    public GameObject Projectile;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
+		
+	}
+}
